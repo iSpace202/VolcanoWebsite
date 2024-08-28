082461e3c05228f94ae59f5a3a917410bf42a37a
iSpace202 <d7oomy220022@gmail.com>
2024-08-28 22:17:06 +0300

    + Maps page sections + Map details update

--- assets/js/populate_maps.js
+++ assets/js/populate_maps.js
@@ -6,10 +6,10 @@ function makeCard(map_data) {
     const map_id = map_data["id"];
 
     return '<div class="col">' +
-        '<div class="card text-bg-light">' +
+        '<div class="card text-bg-light mt-3">' +
         '<img src=' + keyart + ' class="card-img-top" alt="...">' +
-        '<div class="card-body">' + '<span class="badge bg-secondary">' + type + '</span>' +
-        '<h5 class="card-title">' + name + '</h5>' + '<p class="card-text">' + description + '</p>' +
+        '<div class="card-body">' + '<h5 class="card-title">' + name + '</h5>' +
+         '<span class="badge bg-secondary">' + type + '</span>' + '<p class="card-text">' + description + '</p>' +
         '<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mapModal" data-bs-map-id=' + map_id + '>Learn more <i class="bi bi-eye-fill"></i></button>' +
         '</div>' + '</div>' + '</div>';
 }
@@ -17,25 +17,25 @@ function makeCard(map_data) {
 function makeScreenshots(screenshots) {
     const screenshot_amount = screenshots.length;
 
-    let ss_html = '<div id="carouselExampleIndicators" class="carousel slide">' +
+    let ss_html = '<div id="map-screenshot-carousel" class="carousel slide">' +
         '<div class="carousel-indicators">'
     for (let i = 0; i < screenshot_amount; i++) {
-        ss_html += '<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to=' + i + (i === 0 ? ' class="active" aria-current="true"' : '') + ' aria-label="Screenshot ' + (i + 1) + '"></button>'
+        ss_html += '<button type="button" data-bs-target="#map-screenshot-carousel" data-bs-slide-to=' + i + (i === 0 ? ' class="active" aria-current="true"' : '') + ' aria-label="Screenshot ' + (i + 1) + '"></button>'
     }
     ss_html += '</div>' +
         '<div class="carousel-inner">'
     for (let i = 0; i < screenshot_amount; i++) {
-        ss_html += '<div class=' + (i === 0 ? "carousel-item active" : "carousel-item") + '>' +
+        ss_html += '<div class=' + (i === 0 ? '"carousel-item active"' : '"carousel-item"') + '>' +
             '<img src=' + screenshots[i] + ' class="d-block w-100" alt="Screenshot ' + (i + 1) + '">' +
             '</div>'
     }
 
     ss_html += '</div>' +
-        '<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">' +
+        '<button class="carousel-control-prev" type="button" data-bs-target="#map-screenshot-carousel" data-bs-slide="prev">' +
         '<span class="carousel-control-prev-icon" aria-hidden="true"></span>' +
         '<span class="visually-hidden">Previous</span>' +
         '</button>' +
-        '<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">' +
+        '<button class="carousel-control-next" type="button" data-bs-target="#map-screenshot-carousel" data-bs-slide="next">' +
         '<span class="carousel-control-next-icon" aria-hidden="true"></span>' +
         '<span class="visually-hidden">Next</span>' +
         '</button>' +
@@ -50,8 +50,23 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(response => response.json())
         .then(data => {
             maps = data;
-            data.forEach(function (map_data) {
-                $("#mapsTable").append(makeCard(map_data))
+            const adventure_maps = data.filter(function (entry) {
+                return entry.type === 'Adventure';
+            })
+            const minigame_maps = data.filter(function (entry) {
+                return entry.type === 'Minigame';
+            })
+            const survival_spawn_maps = data.filter(function (entry) {
+                return entry.type === 'Survival Spawn';
+            })
+            adventure_maps.forEach(function (map_data) {
+                $("#adventure-maps").append(makeCard(map_data))
+            })
+            minigame_maps.forEach(function (map_data) {
+                $("#minigame-maps").append(makeCard(map_data))
+            })
+            survival_spawn_maps.forEach(function (map_data) {
+                $("#survival-spawn-maps").append(makeCard(map_data))
             })
         })
 });
@@ -69,7 +84,19 @@ if (mapModal) {
                 mapModal.querySelector('.modal-title').textContent = maps[i]["name"].replace(/"/g, "");
                 mapModal.querySelector('#map-description').innerHTML = maps[i]["long_description"].replace(/"/g, "");
                 mapModal.querySelector('#map-screenshots').innerHTML = makeScreenshots(maps[i]["screenshots"]);
-                mapModal.querySelector('#map-trailer').innerHTML = maps[i]["trailer"].length > 5 ? maps[i]["trailer"] : null;
+                mapModal.querySelector('#map-price').innerHTML = maps[i]["price"].replace(/"/g, "");
+                mapModal.querySelector('#map-keyart').setAttribute('src', maps[i]["keyart_path"]);
+                mapModal.querySelector('#map-link').setAttribute("href", `https://www.minecraft.net/en-us/marketplace/pdp?id=${maps[i]["id"]}`);
+                
+                if (maps[i]["trailer"] != null) {
+                    mapModal.querySelector('#map-trailer > iframe').setAttribute("src", `https://www.youtube.com/embed/${maps[i]["trailer"]}`);
+                    mapModal.querySelector('#map-trailer').classList.remove("collapse");
+                    mapModal.querySelector('#map-keyart').classList.add("collapse");
+                }
+                else {
+                    mapModal.querySelector('#map-keyart').classList.remove("collapse");
+                    mapModal.querySelector('#map-trailer').classList.add("collapse");
+                }
 
                 document.title = maps[i]["name"].replace(/"/g, "");
                 history.pushState(null, null, `minecraft-maps/${maps[i]["url_name"].replace(/"/g, "")}`);
@@ -79,12 +106,12 @@ if (mapModal) {
     })
     mapModal.addEventListener('hide.bs.modal', event => {
 
-        history.replaceState(null, null, 'minecraft-maps/');
+        history.replaceState(null, null, '/minecraft-maps');
     })
 }
 
 window.addEventListener("popstate", event => {
-    if (mapModal){
+    if (mapModal) {
         document.title = "Minecraft Maps";
         mapModal.hide();
     }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,99 @@
-// Load header and footer
 
 document.addEventListener("DOMContentLoaded", function () {
     $(function () {
+        // Load header and footer
         $("#header").load("header.html");
         $("#footer").load("footer.html");
+    
+        // Set active header
+        const url_name = window.location.href;
+        {
+            let header_elem;
+
+            if (url_name.includes("games")) {
+                header_elem = document.getElementById("header-games");
+            }
+            else if (url_name.includes("minecraft-maps")) {
+                header_elem = document.getElementById("header-minecraft-maps");
+            }
+            else if (url_name.includes("about")) {
+                header_elem = document.getElementById("header-about");
+            }
+            else if (url_name.includes("contact-us")) {
+                header_elem = document.getElementById("header-contact-us");
+            }
+            else {
+                header_elem = document.getElementById("header-home");
+            }
+            header_elem.classList.add("active");
+            header_elem.setAttribute("aria-current", "page");
+        }
     });
 });
 
 
 // Animate background
-const tl = new TimelineMax({repeat: -1});
+const tl = new TimelineMax({ repeat: -1 });
 
 tl.fromTo(
     "#bubble5",
-    {y: "100vh", opacity: 1},
+    { y: "100vh", opacity: 1 },
     {
         y: "-25vh", opacity: 1,
         duration: 3
     })
     .fromTo(
         "#bubble1",
-        {y: "100vh", opacity: 1},
+        { y: "100vh", opacity: 1 },
         {
             y: "-25vh", opacity: 1,
             duration: 5
         }, "-=3.5")
     .fromTo(
         "#bubble4",
-        {y: "100vh", opacity: 1},
+        { y: "100vh", opacity: 1 },
         {
             y: "-30vh", opacity: 1,
             duration: 7
         }, "-=2.5")
     .fromTo(
         "#bubble8",
-        {y: "100vh", opacity: 1},
+        { y: "100vh", opacity: 1 },
         {
             y: "-25vh", opacity: 1,
             duration: 5
         }, "-=6.5")
     .fromTo(
         "#bubble9",
-        {y: "100vh", opacity: 1},
+        { y: "100vh", opacity: 1 },
         {
             y: "-25vh", opacity: 1,
             duration: 3
         }, "-=4.5")
     .fromTo(
         "#bubble2",
-        {y: "100vh", opacity: 1},
+        { y: "100vh", opacity: 1 },
         {
             y: "-25vh", opacity: 1,
             duration: 3
         }, "-=2.5")
     .fromTo(
         "#bubble6",
-        {y: "100vh", opacity: 1},
+        { y: "100vh", opacity: 1 },
         {
             y: "-25vh", opacity: 1,
             duration: 3
         }, "-=4.5")
     .fromTo(
         "#bubble7",
-        {y: "100vh", opacity: 1},
+        { y: "100vh", opacity: 1 },
         {
             y: "-25vh", opacity: 1,
             duration: 5
         }, "-=2.5")
     .fromTo(
         "#bubble3",
-        {y: "100vh", opacity: 1},
+        { y: "100vh", opacity: 1 },
         {
             y: "-25vh", opacity: 1,
             duration: 3
